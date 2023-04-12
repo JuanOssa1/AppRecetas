e9e32a430a17dc1a0c08c5af0b93c924b151ab07
Juan Ossa Ossa <juan.ossao@perficient.com>
2023-04-12 10:00:39 -0500

    Modal cambiado a la pagina de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import AppHeader from './components/AppHeader/AppHeader';
 import AppMainPage from './components/AppMainPage/AppMainPage';
 import AppFooter from './components/AppFooter/AppFooter';
+import Modal from './components/UI/Modal/Modal';
+import Input from './components/UI/Input/Input';
+import Select from './components/UI/Select/Select';
+import Button from './components/UI/Button/Button';
 import useFetch from './hooks/use-fetch';
+import * as constants from './constants/constants';
 import { useEffect } from 'react';
-import './App.scss';
+import styles from './App.module.scss';
 import { useState } from 'react';
 function App() {
+  const [modalAddRecipe, setModalSetRecipe] = useState(false);
   const [recipes, setRecipes] = useState([]);
   const getRecipes = (recipes) => {
     setRecipes(Object.values(recipes));
@@ -25,13 +31,52 @@ function App() {
       body: recipe,
     });
   };
+  const showModalHandler = () => {
+    setModalSetRecipe((prevState) => !prevState);
+  };
 
   useEffect(() => {
     getRecipesHook();
   }, []);
   return (
-    <div className="App">
-      <AppHeader />
+    <div className={`${styles['App']}`}>
+      {modalAddRecipe && (
+        <Modal onClose={showModalHandler}>
+          <section className={`${styles['modal-form']}`}>
+            <div className={`${styles['modal-form__left']}`}>
+              <Input
+                className={`${styles['input-recipe']}`}
+                label="Nombre de la receta"
+              />
+
+              <Select
+                className={`${styles['select-category']}`}
+                defaultOption="Choose category"
+                id="categories"
+                options={Object.values(constants.categories)}
+              />
+              <Select
+                className={`${styles['select-time']}`}
+                defaultOption="Choose time"
+                id="CookingTime"
+                options={Object.values(constants.cookingTime)}
+              />
+              <Input
+                className={`${styles['input-image']}`}
+                label="Link de imagen"
+              />
+            </div>
+            <div className={`${styles['modal-form__left']}`}>
+              <Input
+                className={`${styles['input-steps']}`}
+                label="Pasos de la receta"
+              />
+              <Button onClick={showModalHandler} content="Agregar" />
+            </div>
+          </section>
+        </Modal>
+      )}
+      <AppHeader addRecipeHandler={showModalHandler} />
       <AppMainPage cardToRender={recipes} sendRecipes={sendRecipesHook} />
       <AppFooter />
     </div>
--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -2,60 +2,16 @@ import React from 'react';
 import styles from './AppHeader.module.scss';
 import Select from '../UI/Select/Select';
 import Button from '../UI/Button/Button';
-import Modal from '../UI/Modal/Modal';
-import Input from '../UI/Input/Input';
 
 import { useState } from 'react';
 import * as constants from '../../constants/constants';
 
-function AppHeader() {
-  const [modalAddRecipe, setModalSetRecipe] = useState(false);
-
-  const showModalHandler = () => {
-    setModalSetRecipe((prevState) => !prevState);
-  };
-
+function AppHeader({ addRecipeHandler }) {
   return (
     <>
-      {modalAddRecipe && (
-        <Modal onClose={showModalHandler}>
-          <section className={`${styles['modal-form']}`}>
-            <div className={`${styles['modal-form__left']}`}>
-              <Input
-                className={`${styles['input-recipe']}`}
-                label="Nombre de la receta"
-              />
-
-              <Select
-                className={`${styles['select-category']}`}
-                defaultOption="Choose category"
-                id="categories"
-                options={Object.values(constants.categories)}
-              />
-              <Select
-                className={`${styles['select-time']}`}
-                defaultOption="Choose time"
-                id="CookingTime"
-                options={Object.values(constants.cookingTime)}
-              />
-              <Input
-                className={`${styles['input-image']}`}
-                label="Link de imagen"
-              />
-            </div>
-            <div className={`${styles['modal-form__left']}`}>
-              <Input
-                className={`${styles['input-steps']}`}
-                label="Pasos de la receta"
-              />
-              <Button onClick={showModalHandler} content="Agregar" />
-            </div>
-          </section>
-        </Modal>
-      )}
       <div className={`${styles['main-nav']}`}>
         <h1 className={`${styles['main-nav__title']}`}>Las recetas de Juan</h1>
-        <Button onClick={showModalHandler} content="Agregar Receta" />
+        <Button onClick={addRecipeHandler} content="Agregar Receta" />
         <form className={`${styles['main-nav__form']}`}>
           <Select
             defaultOption="Choose category"
