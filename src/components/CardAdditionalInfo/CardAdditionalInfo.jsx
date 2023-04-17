36b66aa72665641ff4b1a1a6fd25c40ef8ab5a00
Juan Ossa Ossa <juan.ossao@perficient.com>
2023-04-17 10:03:06 -0500

    Modal desplegado de manera adecuada

--- src/components/CardAdditionalInfo/CardAdditionalInfo.jsx
+++ src/components/CardAdditionalInfo/CardAdditionalInfo.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Modal from '../UI/Modal/Modal';
+import Button from '../UI/Button/Button';
 import styles from './CardAdditionalInfo.module.scss';
 
 function CardAdditionalInfo({ info, onClose, srcImage }) {
   return (
     <Modal className={`${styles['info-modal']}`} onClose={onClose}>
-      <p className={`${styles['info-text']}`}>{info}</p>
+      <section className={`${styles['modal-content']}`}>
+        <h1></h1>
+        <p className={`${styles['modal-content__text']}`}>{info}</p>
+        <Button
+          onClick={onClose}
+          content="Cerrar"
+          className={`${styles['modal-content__button']}`}
+        />
+      </section>
+
       <img
         className={`${styles['modal-image']}`}
         src={srcImage}
