78df3eef7de079e48ce2b2f7da25dcf9907ae840
Juan Ossa Ossa <juan.ossao@perficient.com>
2023-04-20 10:14:39 -0500

    ready for store

---
+++ src/store/favorites-slice.js
@@ -0,0 +1,7 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const favoriteSlice = createSlice({
+  name: 'favorites',
+  initialState: { recipes: [] },
+  reducers: {},
+});
---
+++ src/store/store.js
@@ -0,0 +1,4 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import notificationSlice from './notifications-slice';
+import favoriteSlice from './favorites-slice';
